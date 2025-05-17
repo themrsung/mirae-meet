7b934810eba118afd676656aed792c12ff0895c1
Min Jun Sung <themrsung@gmail.com>
2025-05-18 04:19:32 +0900

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,18 @@
 import React from "react";
 import logo from "./logo.svg";
-import "./App.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Landing from "./pages/landing/Landing";
+import About from "./pages/about/About";
+import PageNotFound from "./pages/page-not-found/PageNotFound";
 
 function App() {
-	return (
-		<div className="App">
-			
-		</div>
-	);
+	return <BrowserRouter>
+	<Routes>
+		<Route path="/" element={<Landing />} />
+		<Route path="about" element={<About />} />
+		<Route path="*" element={<PageNotFound />} />
+	</Routes>
+	</BrowserRouter>;
 }
 
 export default App;
